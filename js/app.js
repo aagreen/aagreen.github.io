7a1bec120af56bf9593e29297e4eec08dedd274a
Ali <idyllven@outlook.com>
2015-11-08 17:29:24 -0600

    version 2.0

--- js/app.js
+++ js/app.js
@@ -6,33 +6,33 @@ angular.module("teahouse", ['ngMaterial', 'ngRoute', 'ngAnimate'])
 
         // home page
         .when('/', {
-            templateUrl: 'home.html',
+            templateUrl: 'views/home.html',
             controller: 'homeCtrl'
         })
         .when('/home', {
-            templateUrl: 'home.html',
+            templateUrl: 'views/home.html',
             controller: 'homeCtrl'
         })
     
         // about
         .when('/gallery', {
-            templateUrl: 'gallery.html',
+            templateUrl: 'views/gallery.html',
             controller: 'galleryCtrl'
         })
     
         // submissions
         .when('/menu', {
-            templateUrl: 'menu.html',
-            controller: 'menuCtrl'
+            templateUrl: 'views/menu.html',
+            controller: 'homeCtrl'
         })
     
         // poetry
         .when('/contact', {
-            templateUrl: 'contact.html',
+            templateUrl: 'views/contact.html',
             controller: 'homeCtrl'
         })
 
-	.otherwise('/');    
+		.otherwise('/');    
     
     $mdThemingProvider.theme('default')
         .primaryPalette('green', {
